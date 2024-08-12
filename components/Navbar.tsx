78faf1743d9ede5ea8fd29ef47bef594474d67ca
gufranlazuardi <gufranlazuardi@gmail.com>
2024-08-12 12:52:50 +0700

    fix: navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
   };
   return (
     <>
-      <nav className="flex w-full justify-between bg-[#10335D] px-[5rem] py-2 fixed top-0 z-100">
+      <nav className="flex w-full justify-between bg-[#10335D] px-[5rem] py-2 fixed top-0 z-[1000]">
         {/* gambar */}
         <button onClick={handleLogoClick}>
           <Image
