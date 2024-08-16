7f7b4f5a3b589c7026b29157af63309b77f5207e
gufranlazuardi <gufranlazuardi@gmail.com>
2024-08-16 15:45:02 +0700

    fix: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow pt-[4.8rem] md:pt-[4.5rem] lg:pt-[4.5rem] xl:pt-[4.5rem]">
+          {children}
+        </main>
         {/* <Footer /> */}
       </body>
     </html>
